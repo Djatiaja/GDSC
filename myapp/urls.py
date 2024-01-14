2dc57708069d5ec8278038f37d4c60234aa24b4c
djatiaja <djamgt23@gmail.com>
2024-01-14 09:53:48 +0700

    Create README.md a

--- myapp/urls.py
+++ myapp/urls.py
@@ -5,6 +5,8 @@ from . import views
 urlpatterns=[
     path("", views.index , name="index"),
     path("hsk/<str:id>", views.hsk, name="hsk"),
-    path("test/<str:id>", views.test, name="test")
+    path("test/<str:id>", views.test, name="test"),
+    path('login', views.login, name="login"),
+    path('register', views.register, name="register")
 ]
 #coba
--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import hsk as list_hsk
+from django.contrib.auth.forms import UserCreationForm
 
 
 
@@ -15,5 +16,15 @@ def hsk(request, id):
     return render(request, "hsk.html", {"hsk": hsk, "list":list1})
 
 def test(request, id):
-
     return render(request, "link.html", {"link": id})
+
+def login(request):
+    return render(request, "login.html")
+
+def register(request):
+    form = UserCreationForm()
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        form.save()
+        return render(request, "register.html", {"form": form})
+    return render(request, "register.html", {"form": form})
